b3f66e75dbd7f5b782757b2f7ccde634f80dc0bf
hapliniste <onchonchcorp@gmail.com>
2014-07-11 17:17:48 +0200

    ajout de la voiture
    voiture: prefab, material, script, texture
    background: texture

---
+++ Assets/Prefabs/Voiture_script.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Voiture_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{			
+		Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+		
+		Vector3 relativePos = worldPos - this.transform.position;
+		
+		Debug.Log("WORLD: " + worldPos.ToString());
+		Debug.Log("RELATIVE:" + relativePos.ToString());
+		relativePos.z = 0.0f;
+		relativePos = relativePos.normalized;
+
+		if(Input.GetMouseButton(0))
+		{
+			Debug.DrawLine(transform.position, transform.position + relativePos);
+		}	
+	}
+}
